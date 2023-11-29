85e5e1f6f0ebec6e3e784d5d6d698e25ea30a6a8
JasonLuis <jason20luis@gmail.com>
2023-11-29 10:24:53 -0300

    chore: configured react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <div className="App">
-        TESTE
-      </div>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
